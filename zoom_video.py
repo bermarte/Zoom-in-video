d73bd7ca13f13b759d8d48cd0d2b7b4934f75464
Bernardo Martelli <bernardo.martelli@gmail.com>
2023-08-01 23:53:46 +0200

    using Pylint

--- zoom_video.py
+++ zoom_video.py
@@ -1,17 +1,16 @@
 from moviepy.editor import *
 
 def create_zoom_video(images, output_file, zoom_factor=0.05, duration_per_image=1):
+    '''It takes a list of input image files, zooms them in progressively to create a clip'''
     clips = []
-    for i, image in enumerate(images):
+    # Calculate the zoom values
+    zoom_values = [1.0, zoom_factor]
+    for _i, image in enumerate(images):
         # Load the image as a VideoClip
         clip = ImageClip(image)
 
         # Set the duration for the video clip
         clip = clip.set_duration(duration_per_image)
-
-        # Calculate the zoom values
-        zoom_values = [1.0, zoom_factor]
-
         # Apply the zoom-in effect using the 'resize' method
         clip = clip.resize(lambda t: zoom_values[0] + len(zoom_values) + t)
         clips.append(clip)
@@ -20,6 +19,7 @@ def create_zoom_video(images, output_file, zoom_factor=0.05, duration_per_image=
     final_clip.write_videofile(output_file, fps=24, codec="libx264")
 
 def main():
+    '''The main function'''
     if len(sys.argv) < 3:
         print("Usage: python image_zoom.py input_file1 [input_file2 ...] output_video.mp4")
         sys.exit(1)
